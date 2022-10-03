c61d2fec8469e9d5a5203fe7f5003e1750b62b6e
ktlh <ktlh@CPH-PF3BXQLP>
2022-10-03 07:43:06 +0200

    Add project files.

---
+++ EventService/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using EventService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace EventService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        /// <summary>
+        /// Kaldes af en anden service, når den har brug for at offentliggøre (publishe) et event
+        /// </summary>
+        /// <param name="e">Information om den event, der er opstået</param>
+        [HttpPost]
+        public void RaiseEvent(Event e) {
+            /// TODO: Skriv din kode her - husk også at implementere event-klassen
+        }
+
+        /// <summary>
+        /// Henter events
+        /// </summary>
+        /// <param name="startIndex">Index på det første event der skal hentes</param>
+        /// <param name="antal">Antallet af events der maksimalt skal hentes (der kan være færre)</param>
+        /// <returns></returns>
+        [HttpGet]
+        public List<Event> ListEvents(int startIndex, int antal)
+        {
+            // TODO Skriv din kode her. Du må gerne ændre returtype og parametre, hvis du vil. Koden her er bare tænkt som et udgangspunkt.
+
+            return new();
+        }
+    }
+}
---
+++ EventService/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace EventService.Models
+{
+    /// <summary>
+    /// Information om den event, der er opstået
+    /// Tænk over, hvilke data det er relevant at event'et indeholder
+    /// </summary>
+    public class Event
+    {
+        // TODO: Du skal selv lave en god implementering af event-klassen
+    }
+}
